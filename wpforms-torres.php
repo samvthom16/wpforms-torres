bb8653edb8f946afd65188f01a5b1d4144957eed
unknown <samvthom16@gmail.com>
2020-01-08 14:54:19 +0530

    Send mail

--- wpforms-torres.php
+++ wpforms-torres.php
@@ -10,57 +10,22 @@
 
   function wpf_dev_process_complete( $fields, $entry, $form_data, $entry_id ) {
 
-    $notification = $form_data['settings']['notifications'][4];
-
-    print_r( $notification['subject'] );
-
-    print_r( $notification['message'] );
-
     // Get the full entry object
     $entry = wpforms()->entry->get( $entry_id );
 
     // Fields are in JSON, so we decode to an array
     $entry_fields = json_decode( $entry->fields, true );
 
-    print_r( $entry_fields );
-
-    print_r( $entry_fields[1]['value'] );
-
-    print_r( $entry_fields[27]['value_raw'] );
+    //print_r( $entry_fields );
 
     if( $entry_fields[27]['value_raw'] == '1,2' ){
-      echo "<br>both are selected<br>";
+
+      $notification = $form_data['settings']['notifications'][4];
+
+      $to = $entry_fields[1]['value'];
+
+      wp_mail( $to, $notification['subject'], $notification['message'] );
+
     }
-
-    //print_r( $entry[1] );
-
-    //print_r( $entry[27] );
-
-    /*
-    // Optional, you can limit to specific forms. Below, we restrict output to
-    // form #5.
-    if ( absint( $form_data['id'] ) !== 5 ) {
-        return;
-    }
-
-    // Get the full entry object
-    $entry = wpforms()->entry->get( $entry_id );
-
-    // Fields are in JSON, so we decode to an array
-    $entry_fields = json_decode( $entry->fields, true );
-
-    // Check to see if user selected 'yes' for callback
-    if($entry_fields[6]['value'] === 'Yes') {
-        // Set the hidden field to 'Needs Callback' to filter through entries
-        $entry_fields[7]['value'] = 'Needs Callback';
-    }
-
-    // Convert back to json
-    $entry_fields = json_encode( $entry_fields );
-
-    // Save changes
-    wpforms()->entry->update( $entry_id, array( 'fields' => $entry_fields ) );
-    */
-
-}
+  }
 add_action( 'wpforms_process_complete', 'wpf_dev_process_complete', 10, 4 );
